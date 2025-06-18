32315908b023abc4b89b75fc2de092f3b15b8675
peter <pjkrupa@proton.me>
2025-06-18 11:42:27 +0200

    completes API except for a single bug

--- local_api.py
+++ local_api.py
@@ -1,15 +1,14 @@
 import json
-
 import requests
 
 # TODO: send a GET using the URL http://127.0.0.1:8000
-r = None # Your code here
+r = requests.get("http://127.0.0.1:8000")
 
 # TODO: print the status code
-# print()
-# TODO: print the welcome message
-# print()
+print("GET status code:", r.status_code)
 
+# TODO: print the welcome message
+print("GET response:", r.text)
 
 
 data = {
@@ -30,9 +29,10 @@ data = {
 }
 
 # TODO: send a POST using the data above
-r = None # Your code here
+r = requests.post("http://127.0.0.1:8000/data/", json=data)
 
 # TODO: print the status code
-# print()
+print("POST status code:", r.status_code)
+
 # TODO: print the result
-# print()
+print("POST response:", r.json())
